4ebbd96abeeb380e61de9d0cdd1ae743bef019f6
erdiolmezogullari <erdi.olmez@gmail.com>
2019-09-08 15:23:53 +0300

    Added project name into docker-compose for app and platform.

--- dcompose.sh
+++ dcompose.sh
@@ -1,40 +1,43 @@
 #!/usr/bin/env bash
 
 
+# Creating Network If Not Exist
 create_network_not_exist()
 {
     n=`docker network ls | grep ${NETWORK_NAME} | wc -l`
-    if [ ${n} -eq 0  ]
+    if [[ ${n} -eq 0 ]]
     then
-        # Creating Network
         docker network create ${NETWORK_NAME}
         docker network ls
     fi
 }
 
+# Deleting Network If Exist
 delete_network_exist()
 {
     n=`docker network ls | grep ${NETWORK_NAME} | wc -l`
-    if [ ${n} -eq 1 ]
+    if [[ ${n} -eq 1 ]]
     then
-        # Deleting Network
         docker network rm ${NETWORK_NAME}
     fi
 }
 
 
-if [[ ! ($# -ge 2 &&  $# -le 3) ]]
+if [[ ! ($# -ge 3 &&  $# -le 4) ]]
 then
-    echo "Usage: $0 <build|up|down> <docker_compose_file> <app_env_shell(optional)>"
+    echo "Usage: $0 <build|up|down> <docker_compose_file> <project_name> <app_env_shell(optional)>"
     exit
 fi
 
 COMMAND=$1
 DOCKER_COMPOSE_FILE=$2
-APP_ENV_SHELL=$3
+PROJECT_NAME=$3
+APP_ENV_SHELL=$4
 
+# Loading common environment variables for app and platform
 . ./common_env.sh
 
+# Loading app environment variables
 if [[ ! -z ${APP_ENV_SHELL} ]]
 then
     . ./${APP_ENV_SHELL}
@@ -44,17 +47,18 @@ fi
 case ${COMMAND} in
     build)
         create_network_not_exist
-        docker-compose -f ${DOCKER_COMPOSE_FILE} build;;
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} build;;
     up)
         create_network_not_exist
-        docker-compose -f ${DOCKER_COMPOSE_FILE} up;;
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} up;;
     down)
-        docker-compose -f ${DOCKER_COMPOSE_FILE} down
-        docker-compose -f ${DOCKER_COMPOSE_FILE} stop
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} stop
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} down
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} rm
         delete_network_exist
         ;;
-    *) echo "Usage: $0 <build|up|down> <docker_compose_file>";;
+    ps)
+        docker-compose -f ${DOCKER_COMPOSE_FILE} -p ${PROJECT_NAME} ps;;
+    *)
+        echo "Usage: $0 <build|up|down> <docker_compose_file> <project_name> <app_env_shell(optional)>";;
 esac
-
-
-
--- makefile
+++ makefile
@@ -3,22 +3,27 @@ APP_DOCKER_COMPOSE_FILE:=docker-compose.app-flink.yml
 APP_ENV_SHELL=app_env.sh
 
 build_platform:
-	./dcompose.sh build ${PLATFORM_DOCKER_COMPOSE_FILE}
+	./dcompose.sh build ${PLATFORM_DOCKER_COMPOSE_FILE} platform
 up_platform:
-	./dcompose.sh up ${PLATFORM_DOCKER_COMPOSE_FILE}
-stop_platform:
-	./dcompose.sh down ${PLATFORM_DOCKER_COMPOSE_FILE}
+	./dcompose.sh up ${PLATFORM_DOCKER_COMPOSE_FILE} platform
+down_platform:
+	./dcompose.sh down ${PLATFORM_DOCKER_COMPOSE_FILE} platform
+ps_platform:
+	./dcompose.sh ps ${PLATFORM_DOCKER_COMPOSE_FILE} platform
 
 mvn_package:
 	mvn clean package
-
 build_app: mvn_package
-	./dcompose.sh build ${APP_DOCKER_COMPOSE_FILE} ${APP_ENV_SHELL}
+	./dcompose.sh build ${APP_DOCKER_COMPOSE_FILE} app ${APP_ENV_SHELL}
 up_app:
-	./dcompose.sh up ${APP_DOCKER_COMPOSE_FILE} ${APP_ENV_SHELL}
-stop_app:
-	./dcompose.sh down ${APP_DOCKER_COMPOSE_FILE} ${APP_ENV_SHELL}
+	./dcompose.sh up ${APP_DOCKER_COMPOSE_FILE} app ${APP_ENV_SHELL}
+down_app:
+	./dcompose.sh down ${APP_DOCKER_COMPOSE_FILE} app ${APP_ENV_SHELL}
+ps_app:
+	./dcompose.sh ps ${APP_DOCKER_COMPOSE_FILE} app ${APP_ENV_SHELL}
+
 
 build_all: build_platform build_app
 up_all: up_platform up_app
-stop_all: stop_app stop_platform
+down_all: down_app down_platform
+ps_all: ps_platform ps_app
